1a70a8db759a584089ef6b5e0ef46b23823f54ed
Igor Morzan <igormorzan@dhcp-10-250-217-245.harvard.edu>
2019-04-23 09:34:23 -0400

    edited

--- shotspotter_sf.R/app.R
+++ shotspotter_sf.R/app.R
@@ -9,52 +9,47 @@
 
 library(shiny)
 library(shinythemes)
-library(tidyverse)
-library(readr)
-library(sf)
-library(stringr)
-library(tigris)
-library(lubridate)
-library(gganimate)
-library(ggthemes)
-library(transformr)
-library(png)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(theme = shinytheme("darkly"),
-  
-  tabsetPanel(
-  
-  tabPanel(
-   
-   # Application title
-   titlePanel("Locations of Gunfire incidents in San Francisco, CA (2013 - 2015)"),
-   
-      # Show a plot of the generated distribution
-      mainPanel(
-        width = 7,
-        imageOutput("map_sf")
-      )
-),
-
-  tabPanel("About", textOutput("message"))
-
-))
+                
+                tabsetPanel(
+                  
+                  tabPanel(
+                    
+                    titlePanel("Locations of Gunfire incidents in San Francisco, CA (2013 - 2015)"),
+                    
+                    mainPanel(imageOutput("img")),     
+                    
+                    # Application title
+                    
+                    
+                    # Sidebar with a brief explanation of the plot 
+                    # sidebarLayout(
+                    #   sidebarPanel(
+                    #     div("This plot shows incidents of multiple gunshots detected by the ShotSpotter program in San Francisco.")
+                    #   )
+                    # ),      
+                    # Show a plot of the generated distribution
+                    # ),
+                    
+                    tabPanel("About", textOutput("message"))
+                  )))
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-   
-  output$map_sf <- renderImage({
+  
+  output$img <- renderImage({
     list(src = "sf.gif",
-    contentType = "image/gif")
-    }, 
-    deleteFile = FALSE)
+         contentType = "image/gif")
+  }, 
+  deleteFile = FALSE)
   
   output$message <- renderText({"This project was made with data from the ShotSpotter project, thanks to the Justice Tech Lab.
     Our code for this project can be found at https://github.com/imorzan/shotspotter_sf."
-    })
-   
-}
+  })
+  
+  }
 
 # Run the application 
 shinyApp(ui = ui, server = server)
