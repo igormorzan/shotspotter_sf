bb5f326fb4624497bcea324549be61b2e24b6d31
Igor Morzan <igormorzan@dhcp-10-250-217-245.harvard.edu>
2019-04-23 09:30:41 -0400

    edited

--- shotspotter_sf.R/app.R
+++ shotspotter_sf.R/app.R
@@ -9,7 +9,6 @@
 
 library(shiny)
 library(shinythemes)
-<<<<<<< HEAD
 library(tidyverse)
 library(readr)
 library(sf)
@@ -23,41 +22,6 @@ library(png)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(theme = shinytheme("darkly"),
-                
-                tabsetPanel(
-                  
-                  tabPanel("Plot",
-                    
-                    # Application title
-                    titlePanel("Locations of Gunfire incidents in San Francisco, CA (2013 - 2015)"),
-                    # Show a plot of the generated distribution
-                    mainPanel(
-                      width = 7,
-                      imageOutput("map_sf")
-                    )
-                  ),
-                  
-                  tabPanel("About", textOutput("message"))
-                ))
-
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-  
-  output$map_sf <- renderImage({
-    list(src = "sf.gif",
-         contentType = "image/gif")
-  }, 
-  deleteFile = FALSE)
-  
-  output$message <- renderText({"This project was made with data from the ShotSpotter project, thanks to the Justice Tech Lab.
-    Our code for this project can be found at https://github.com/imorzan/shotspotter_sf."
-  })
-  
-  }
-=======
-
-# Define UI for application that draws a histogram
-ui <- fluidPage(theme = shinytheme("darkly"),
   
   tabsetPanel(
   
@@ -66,13 +30,6 @@ ui <- fluidPage(theme = shinytheme("darkly"),
    # Application title
    titlePanel("Locations of Gunfire incidents in San Francisco, CA (2013 - 2015)"),
    
-   # Sidebar with a brief explanation of the plot 
-   sidebarLayout(
-      sidebarPanel(
-        div(style="text-align:center","This plot shows incidents of",br(), "multiple gunshots",br(),
-            "detected by the ShotSpotter",br(),"program in San Francisco.")
-      )
-   ),      
       # Show a plot of the generated distribution
       mainPanel(
         width = 7,
@@ -98,7 +55,6 @@ server <- function(input, output) {
     })
    
 }
->>>>>>> 1444732f60d0a0b8bfcf7f236a7562b22f1e52ab
 
 # Run the application 
 shinyApp(ui = ui, server = server)
